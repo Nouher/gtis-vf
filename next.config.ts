ad9715aae84c093dab802dacbc7272999efb9552
Hamza Nouher <nouhirh@gmail.com>
2025-10-09 01:36:16 +0100

    fix slider home page

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 
 const nextConfig: NextConfig = {
-  output: 'export', // for static export
+//  output: 'export', // for static export
   images: {
     unoptimized: true, // disable image optimization for static export
   },
