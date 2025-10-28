ab2d41a19805a8dadc40893c3e0ddb52178a4828
Hamza Nouher <nouhirh@gmail.com>
2025-10-28 22:19:19 +0100

    edit config file

--- next.config.ts
+++ next.config.ts
@@ -6,5 +6,15 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true, // disable image optimization for static export
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "gtis.ma" }],
+        destination: "https://www.gtis.ma/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 export default nextConfig;
